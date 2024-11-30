051113f0eb3ea31a0263d5002f94ff53507ede85
veen0219 <veen0219@naver.com>
2024-11-30 15:36:42 +0900

    [FEAT] 기타23

--- lib/pages/receivedLetterBoxPage.dart
+++ lib/pages/receivedLetterBoxPage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:hy_thon_team3/pages/sendedLetterboxPage.dart';
 import 'package:intl/intl.dart';
-
+import '../components/DateSelector.dart';
 class ReceivedLetterBoxPage extends StatefulWidget {
   const ReceivedLetterBoxPage({Key? key}) : super(key: key);
 
@@ -12,14 +12,56 @@ class ReceivedLetterBoxPage extends StatefulWidget {
 }
 
 class _ReceivedLetterBoxPageState extends State<ReceivedLetterBoxPage> {
-  DateTime currentMonth = DateTime.now();
-  DateTime selectedDate = DateTime.now();
+  DateTime currentMonth = DateTime.now(); // 현재 표시 중인 월
+  DateTime selectedDate = DateTime.now(); // 선택된 날짜
+
+  // 현재 월의 첫 번째 날
+  DateTime get firstDayOfMonth => DateTime(currentMonth.year, currentMonth.month, 1);
+
+  // 현재 월의 마지막 날
+  DateTime get lastDayOfMonth =>
+      DateTime(currentMonth.year, currentMonth.month + 1, 0);
+
+  // 현재 월의 전체 날짜 (빈칸 포함)
+  List<DateTime> get daysInMonth {
+    final daysBefore = firstDayOfMonth.weekday % 7; // 첫 날 앞의 빈칸 수
+    final daysAfter = 7 - (lastDayOfMonth.weekday % 7) - 1; // 마지막 날 뒤의 빈칸 수
+    final totalDays = firstDayOfMonth.subtract(Duration(days: daysBefore));
+    final days = List.generate(
+      daysBefore + lastDayOfMonth.day + daysAfter,
+          (index) => totalDays.add(Duration(days: index)),
+    );
+    return days;
+  }
 
   // 편지 데이터를 저장할 변수
   String? diaryTitle;
   String? diaryContent;
   bool isLoading = true; // 로딩 상태
   bool hasError = false; // 에러 상태
+
+  void _onDateSelected(DateTime date) {
+    setState(() {
+      selectedDate = date;
+      _fetchDiary(selectedDate); // 날짜 변경 시 API 호출
+    });
+    Navigator.pop(context); // 캘린더 닫기
+  }
+
+// _previousDay와 _nextDay도 API 호출 포함
+  void _previousDay() {
+    setState(() {
+      selectedDate = selectedDate.subtract(const Duration(days: 1));
+      _fetchDiary(selectedDate); // 이전 날짜에 대한 API 호출
+    });
+  }
+
+  void _nextDay() {
+    setState(() {
+      selectedDate = selectedDate.add(const Duration(days: 1));
+      _fetchDiary(selectedDate); // 다음 날짜에 대한 API 호출
+    });
+  }
 
   @override
   void initState() {
@@ -79,15 +121,6 @@ class _ReceivedLetterBoxPageState extends State<ReceivedLetterBoxPage> {
       });
     }
   }
-
-  void _onDateSelected(DateTime date) {
-    setState(() {
-      selectedDate = date;
-    });
-    _fetchDiary(date); // 새로운 날짜에 대한 API 호출
-    Navigator.pop(context); // 캘린더 닫기
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -128,6 +161,14 @@ class _ReceivedLetterBoxPageState extends State<ReceivedLetterBoxPage> {
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 15),
+            DateSelector(
+              date: DateFormat('yyyy/MM/dd').format(selectedDate),
+              onPrevious: _previousDay, // 하루 전으로 이동
+              onNext: _nextDay, // 하루 후로 이동
+              onTap: () => _showCalendar(context), // 클릭 시 캘린더 표시
+              showArrows: true, // WriteDiaryPage에서는 화살표 숨김
+            ),
+            const SizedBox(height: 16),
             Card(
               color: Colors.white,
               elevation: 2,
@@ -139,52 +180,264 @@ class _ReceivedLetterBoxPageState extends State<ReceivedLetterBoxPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
+                    // 날짜와 아이콘을 포함한 Row
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: 110, // 원하는 너비
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFFFF4E0), // 베이지색 배경
+                          borderRadius: BorderRadius.circular(4), // 약간의 모서리 둥글게
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬
+                          children: [
+                            const Text(
+                              '🕐',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            const SizedBox(width: 5), // 아이콘과 날짜 간격
+                            Text(
+                              DateFormat('yy/MM/dd').format(selectedDate), // 날짜 텍스트
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16), // 날짜와 제목 사이 간격
+                    // 편지 제목
                     Text(
-                      diaryTitle ?? '받은 편지가 없습니다.',
+                      diaryTitle ?? '받은 편지가 없습니다.', // 제목 표시
                       style: const TextStyle(
                         fontSize: 18,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    const SizedBox(height: 16),
-                    Text(
-                      diaryContent ?? '',
-                      style: const TextStyle(fontSize: 14),
-                    ),
-                    const SizedBox(height: 16),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        onPressed: diaryTitle != null
-                            ? () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SendedLetterBoxPage(),
+                    const SizedBox(height: 16), // 제목과 본문 사이 간격
+                    // 본문과 버튼은 편지가 있을 때만 표시
+                    if (diaryContent != null) ...[
+                      Text(
+                        diaryContent ?? '', // 본문 텍스트
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                      const SizedBox(height: 16), // 본문과 버튼 사이 간격
+                      Align(
+                        alignment: Alignment.centerRight, // 버튼 오른쪽 정렬
+                        child: ElevatedButton(
+                          onPressed: diaryTitle != null
+                              ? () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const SendedLetterBoxPage(),
+                              ),
+                            );
+                          }
+                              : null, // 제목이 없으면 버튼 비활성화
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: diaryTitle != null ? Colors.black : Colors.grey, // 활성화 여부에 따른 색상
+                            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
                             ),
-                          );
-                        }
-                            : null,
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: diaryTitle != null ? Colors.black : Colors.grey,
-                          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 12),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: const Text(
+                            '내 편지 보러 가기',
+                            style: TextStyle(color: Colors.white),
                           ),
                         ),
-                        child: const Text(
-                          '내 편지 보러 가기',
-                          style: TextStyle(color: Colors.white),
-                        ),
                       ),
-                    ),
+                    ],
                   ],
                 ),
               ),
-            ),
+            )
+
           ],
         ),
       ),
     );
   }
+
+  Widget _buildWeekDays() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: const [
+        Text('SUN', style: TextStyle(color: Colors.grey, fontSize: 14)),
+        Text('MON', style: TextStyle(color: Colors.grey, fontSize: 14)),
+        Text('TUE', style: TextStyle(color: Colors.grey, fontSize: 14)),
+        Text('WED', style: TextStyle(color: Colors.grey, fontSize: 14)),
+        Text('THU', style: TextStyle(color: Colors.grey, fontSize: 14)),
+        Text('FRI', style: TextStyle(color: Colors.grey, fontSize: 14)),
+        Text('SAT', style: TextStyle(color: Colors.grey, fontSize: 14)),
+      ],
+    );
+  }
+  void _showCalendar(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      backgroundColor: Colors.transparent,
+      builder: (BuildContext context) {
+        return DraggableScrollableSheet(
+          initialChildSize: 0.6,
+          maxChildSize: 0.9,
+          minChildSize: 0.3,
+          builder: (_, controller) {
+            return StatefulBuilder(
+              builder: (BuildContext context, StateSetter setModalState) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      topRight: Radius.circular(20),
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.2),
+                        blurRadius: 10,
+                        offset: const Offset(0, -5),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 6,
+                        margin: const EdgeInsets.symmetric(vertical: 8),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      // 달력 헤더
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                setModalState(() {
+                                  currentMonth = DateTime(
+                                    currentMonth.year,
+                                    currentMonth.month - 1,
+                                  );
+                                });
+                              },
+                              icon: const Icon(Icons.chevron_left),
+                            ),
+                            Text(
+                              DateFormat('MMMM yyyy').format(currentMonth),
+                              style: const TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                setModalState(() {
+                                  currentMonth = DateTime(
+                                    currentMonth.year,
+                                    currentMonth.month + 1,
+                                  );
+                                });
+                              },
+                              icon: const Icon(Icons.chevron_right),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 8),
+                      // 요일 표시
+                      _buildWeekDays(),
+                      const SizedBox(height: 16),
+                      Expanded(
+                        child: GridView.builder(
+                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 7, // 7일 주
+                            mainAxisSpacing: 10,
+                            crossAxisSpacing: 10,
+                          ),
+                          itemCount: daysInMonth.length,
+                          itemBuilder: (context, index) {
+                            final day = daysInMonth[index];
+                            final isCurrentMonth = day.month == currentMonth.month;
+                            final isSelected = day == selectedDate;
+
+                            return GestureDetector(
+                              onTap: isCurrentMonth
+                                  ? () {
+                                setState(() {
+                                  selectedDate = day; // DateSelector와 동기화
+                                });
+                                Navigator.pop(context);
+                              }
+                                  : null,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Container(
+                                    alignment: Alignment.center,
+                                    decoration: BoxDecoration(
+                                      color: isSelected
+                                          ? const Color(0xFFEBB161)
+                                          : Colors.transparent,
+                                      shape: BoxShape.circle, // 동그라미 형태
+                                    ),
+                                    width: 30, // 동그라미 크기 조정
+                                    height: 30,
+                                    child: Text(
+                                      '${day.day}',
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                        fontWeight: FontWeight.bold,
+                                        color: isCurrentMonth
+                                            ? (isSelected
+                                            ? Colors.black
+                                            : Colors.grey[800])
+                                            : Colors.grey,
+                                      ),
+                                    ),
+                                  ),
+                                  if (day.weekday == DateTime.sunday) // 빨간 점 추가
+                                    const Padding(
+                                      padding: EdgeInsets.only(top: 4.0),
+                                      child: Icon(
+                                        Icons.circle,
+                                        size: 4,
+                                        color: Colors.red,
+                                      ),
+                                    ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          },
+        );
+      },
+    );
+  }
 }
+
